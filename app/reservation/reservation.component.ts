898773dc479611e2e6345f9e7ad29fe54e669224
wjkim81 <onejean81@gmail.com>
2018-06-22 04:13:18 +0900

    Modal Dialog

--- app/reservation/reservation.component.ts
+++ app/reservation/reservation.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Inject, ChangeDetectorRef, ViewContainerRef } from '@angular/core';
 import { DrawerPage } from '../shared/drawer/drawer.page';
 import { TextField } from 'ui/text-field';
 import { Switch } from 'ui/switch';
 import { Validators, FormBuilder, FormGroup} from '@angular/forms';
+import { ModalDialogService, ModalDialogOptions } from "nativescript-angular/modal-dialog";
+import { ReservationModalComponent } from "../reservationmodal/reservationmodal.component";
 
 @Component({
   selector: 'app-reservation',
@@ -15,7 +17,9 @@ export class ReservationComponent extends DrawerPage implements OnInit {
 
   constructor(
     private changeDetectorRef: ChangeDetectorRef,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+    private modalService: ModalDialogService, 
+    private vcRef: ViewContainerRef) {
       super(changeDetectorRef);
 
       this.reservation = this.formBuilder.group({
@@ -53,5 +57,24 @@ export class ReservationComponent extends DrawerPage implements OnInit {
 
   onSubmit() {
     console.log(JSON.stringify(this.reservation.value));
+  }
+
+  createModalView(args) {
+
+    let options: ModalDialogOptions = {
+        viewContainerRef: this.vcRef,
+        context: args,
+        fullscreen: false
+    };
+
+    this.modalService.showModal(ReservationModalComponent, options)
+      .then((result: any) => {
+          if (args === "guest") {
+              this.reservation.patchValue({guests: result});
+          }
+          else if (args === "date-time") {
+              this.reservation.patchValue({ dateTime: result});
+          }
+    });
   }
 }
